5d1a76d24669419ed11e7a435c62ee99641716b0
kallidl <3046507560@qq.com>
2024-09-17 11:05:30 +0800

    使用RegistrationBean注册Java Web的三大组件

--- src/main/java/com/example/demo3/config/WebConfigure.java
+++ src/main/java/com/example/demo3/config/WebConfigure.java
@@ -4,25 +4,33 @@ import com.example.demo3.web.FirstServlet;
 import com.example.demo3.web.MyFilter;
 import com.example.demo3.web.MyListener;
 import com.example.demo3.web.SecondServlet;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
 @Configuration
 
 public class WebConfigure {
-   @Bean("firstServlet")
-    public FirstServlet firstServlet(){
-        return  new FirstServlet();
-    }
-    @Bean("secondServlet")
-    public SecondServlet secondServlet(){
-        return  new SecondServlet();
+    @Bean
+    public ServletRegistrationBean fistServlet(){
+        return  new ServletRegistrationBean(new FirstServlet(),"/first");
     }
     @Bean
-    public MyFilter myFilter(){
-        return  new MyFilter();
+    public ServletRegistrationBean secondServlet(){
+        return  new ServletRegistrationBean(new SecondServlet(),"/second");
     }
     @Bean
-    public MyListener myListener(){
-        return  new MyListener();
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new MyFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/first"));
+        return  filterRegistrationBean;
+    }
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        return  new ServletListenerRegistrationBean(new MyListener());
     }
 }
